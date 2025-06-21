6d6ccaa765fa17070ea0427c2c07d46fb9d3f096
Rayyan Ali Shah <rs08770@st.habib.edu.pk>
2025-06-21 13:43:05 +0400

    added drug likeness filteration systems

--- get_lipsinki_properties.py
+++ get_lipsinki_properties.py
@@ -27,9 +27,79 @@ def fetch_all_compound_properties():
             print(f"Failed to fetch properties for CID {cid}")
         time.sleep(0.4)
     # Save detailed JSON
-    with open("ligands_lipinski_properties.json", "w") as f:
+    with open("ligands_lipsinki_properties.json", "w") as f:
         json.dump(all_props, f, indent=2)
 
+def num_violations():
+    '''
+    Filterations will be done based on the following guidelines:
+    mw =< 500
+    hacceptor =< 10
+    hdonor =< 5
+    rotatable =< 10
+    20=<tpsa=<130
+    xlogp =< 5
+    '''
+    guides = {
+        "MolecularWeight": 500,
+        "HBondAcceptorCount": 10,
+        "HBondDonorCount": 5,
+        "RotatableBondCount": 10,
+        "TPSA": (20, 130),
+        "XLogP": 5
+    }
+    with open('ligands_lipsinki_properties.json', 'r') as f:
+        all_props = json.load(f)
+    violation_counts = []
+    for cid, props in all_props.items():
+        failed_properties = []
+        violations = 0
+        title = props.get('Title')
+        mw = float(props.get('MolecularWeight'))
+        hacceptor = props.get('HBondAcceptorCount')
+        hdonor = props.get("HBondDonorCount")
+        rotatable = props.get("RotatableBondCount")
+        tpsa = props.get("TPSA")
+        xlogp = props.get("XLogP")
+        if mw > guides["MolecularWeight"]:
+            violations += 1
+            failed_properties.append("MolecularWeight")
+        if hacceptor > guides["HBondAcceptorCount"]:
+            violations += 1 
+            failed_properties.append("HBondAcceptorCount")
+        if hdonor > guides["HBondDonorCount"]:
+            violations += 1
+            failed_properties.append("HBondDonorCount")
+        if rotatable > guides["RotatableBondCount"]:
+            violations += 1
+            failed_properties.append("RotatableBondCount")
+        if tpsa < guides["TPSA"][0] or tpsa > guides["TPSA"][1]:
+            violations += 1
+            failed_properties.append("TPSA")
+        if xlogp > guides["XLogP"]:
+            violations += 1
+            failed_properties.append("XLogP")
+        violation_counts.append((title, cid, violations, failed_properties))
+    return violation_counts
+
+
+def early_drug_filteration(allowed=3):
+    num_violations_list = num_violations()
+    discarded_cids = []
+    for title, cid, violations, failed_properties in num_violations_list:
+        if violations > allowed:
+            discarded_cids.append((title, cid, violations, failed_properties))
+    print(f"Discarded {len(discarded_cids)} compounds with more than {allowed} violations.")
+    with open("primary_lipsinki_discarded_compounds.json", "w") as f:
+        json.dump(discarded_cids, f, indent=2)
+    with open('FilteredCompounds.txt', 'w') as f:
+        listCIDs = get_CIDs()
+        for cid in listCIDs:
+            if cid not in [item[1] for item in discarded_cids]:
+                f.write(f"CID:{cid}\n")
+            
+
 if __name__ == "__main__":
-    fetch_all_compound_properties()
-    print("All properties fetched and saved to ligands_lipinski_properties.json")
+    # fetch_all_compound_properties()
+    # print("All properties fetched and saved to ligands_lipinski_properties.json")
+    early_drug_filteration()
